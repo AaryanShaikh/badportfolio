42da62be4b82a7752f1f91a9f37f33c457c86313
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2021-10-30 20:58:12 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import About from './components/About';
+
 
 export default class App extends Component {
   constructor() {
@@ -17,23 +20,28 @@ export default class App extends Component {
   render() {
     return (
       <div className="App" >
-        <div className="header">
-          <div className="logo">
-            <img src="https://github.com/AaryanShaikh/badportfolio/blob/main/src/res/logo.png?raw=true" alt="" />
+        <Router>
+          <div className="header">
+            <div className="logo">
+              <img src="https://github.com/AaryanShaikh/badportfolio/blob/main/src/res/logo.png?raw=true" alt="" />
+            </div>
+            <div className="nav">
+              <Link to="/"><p><span>H</span>ome</p></Link>
+              <Link to="/about"><p><span>A</span>bout</p></Link>
+              <p><span>P</span>rojects</p>
+              <p><span>C</span>ontact</p>
+            </div>
+            <ul onClick={this.hamToggle} className={this.state.isHamActive ? "active" : ""}>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
           </div>
-          <div className="nav">
-            <p><span>H</span>ome</p>
-            <p><span>A</span>bout</p>
-            <p><span>P</span>rojects</p>
-            <p><span>C</span>ontact</p>
-          </div>
-          <ul onClick={this.hamToggle} className={this.state.isHamActive ? "active" : ""}>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <Home />
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/about"><About /></Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,13 @@
 import React, { Component } from 'react'
-import LetterMorph from 'react-letter-morph';
 import './Home.css';
 
 export default class Home extends Component {
+    componentDidMount = () => {
+        window.distort()
+    }
     render() {
         return (
             <div className="home">
-                {/* <LetterMorph
-                    words={['Frontend', 'React']}
-                    colors={['#ffffff', '#2eac68']}
-                    height={100}
-                    width={170}
-                    fontSize={50}
-                    speed={0.5}
-                    period={3}
-                /> */}
                 <div className="left"></div>
                 <div className="center">
 
