6976fd7e84e9e641cdab8f45fa520d8c3374c64e
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2021-11-02 14:40:59 +0530

    6

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 import './About.css';
 
 export default class About extends Component {
+    componentDidMount = () => {
+        window.mouseFollow()
+    }
     render() {
         return (
             <div className="about">
-
+                <div className="container">
+                    <div className="left"></div>
+                    <div className="right"></div>
+                </div>
             </div>
         )
     }
